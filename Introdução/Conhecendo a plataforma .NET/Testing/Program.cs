eaf76f6b31cd5e638b7cf1767edea18c57114a19
ThiagoKrz <thiagothisago@gmail.com>
2022-10-31 11:52:42 -0400

    Arquivos de estudos iniciais em C#

---
+++ Introdução/Conhecendo a plataforma .NET/Testing/Program.cs
@@ -0,0 +1,52 @@
+﻿using Testing.Models;
+
+string a = "10-";
+int b = 25;
+int.TryParse(a, out b);
+
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso");
+// int b = int.MaxValue;
+// double a = Convert.ToInt32(b);
+// Console.WriteLine(b);
+
+// int a = 5;
+// int b = 9;
+// int inteiro = a+b;
+// string segs = inteiro.ToString()+" vezes";
+// Console.WriteLine(segs);
+
+
+// int a = 10;
+// int b = 20;
+// int c = a+b;
+// // c = c + 5;
+// // c = c - 5;
+// // c += 5;
+// // c -= 5;
+// c*= 5;
+
+// Console.WriteLine(c);
+
+
+// DateTime dataAtual= DateTime.Now.AddDays(5).AddHours(20);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// string salve = "Opa, salve salve, maninho!";
+// int quantidade = 2;
+// double altura = 1.8;
+// decimal valor = 10.80M;
+// bool condicao = true;
+// Console.WriteLine(salve);
+// Console.WriteLine("Valor da variável quantidade: "+quantidade);
+// Console.WriteLine("Valor da variável altura: "+altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável valor: "+valor);
+// Console.WriteLine("Valor da variável condição: "+condicao);
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome="Bruno Valentim";
+//p1.Idade=22;
+//p1.Apresentar();
+
